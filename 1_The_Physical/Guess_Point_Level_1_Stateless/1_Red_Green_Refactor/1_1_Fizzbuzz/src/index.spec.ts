3ac3badcbb294e86866d63bc0c5b60c58b5e0de1
msantos48 <msantos48@gatech.edu>
2023-03-20 16:06:58 -0400

    [New]: returns Fizz for multiples of 3

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -8,5 +8,11 @@ describe("fizzbuzz", () => {
   it("returns the number as a string", () => {
     expect(fizzbuzz(5)).toBe('5');
   });
+
+  it("returns Fizz for multiples of 3", () => {
+    for(let i = 3; i < 100; i += 3) {
+      expect(fizzbuzz(i)).toBe('Fizz');
+    }
+  });
   
 });
