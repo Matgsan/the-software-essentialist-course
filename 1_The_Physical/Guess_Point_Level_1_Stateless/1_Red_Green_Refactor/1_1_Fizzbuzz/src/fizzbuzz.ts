6c4741441bb67d1f5af4becbf0c25782ad915996
msantos48 <msantos48@gatech.edu>
2023-03-20 16:25:12 -0400

    [New]: returns Buzz for multiples of 5 and not 3

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,5 @@
 export default function fizzbuzz(n: number): string {
+  if(n % 5 === 0) return 'Buzz';
   if(n % 3 === 0) return 'Fizz';
   return n.toString();
 }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -6,13 +6,20 @@ describe("fizzbuzz", () => {
   });
 
   it("returns the number as a string", () => {
-    expect(fizzbuzz(5)).toBe('5');
+    expect(fizzbuzz(1)).toBe('1');
   });
 
   it("returns Fizz for multiples of 3 and not multiples of 5", () => {
     for(let i = 3; i < 100; i += 3) {
       if(i % 5 === 0) continue;
       expect(fizzbuzz(i)).toBe('Fizz');
+    }
+  });
+
+  it("returns Buzz for multiples of 5 and not multiples of 3", () => {
+    for(let i = 5; i < 100; i += 5) {
+      if(i % 3 === 0) continue;
+      expect(fizzbuzz(i)).toBe('Buzz');
     }
   });
   
