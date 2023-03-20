0597f1b18364cf934ac0ae2bb6828a121e6aa0f2
msantos48 <msantos48@gatech.edu>
2023-03-20 16:24:08 -0400

    [improvement]: return Fizz only if is multiple of 3 and not 5

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -9,8 +9,9 @@ describe("fizzbuzz", () => {
     expect(fizzbuzz(5)).toBe('5');
   });
 
-  it("returns Fizz for multiples of 3", () => {
+  it("returns Fizz for multiples of 3 and not multiples of 5", () => {
     for(let i = 3; i < 100; i += 3) {
+      if(i % 5 === 0) continue;
       expect(fizzbuzz(i)).toBe('Fizz');
     }
   });
